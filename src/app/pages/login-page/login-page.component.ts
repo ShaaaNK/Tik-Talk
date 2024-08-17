f067c4fb2adc760d15ca5ecb385dae634aa0290b
Сергей Захарьев <sergeyz@MacBook-Pro-Sergej.local>
2024-08-17 17:58:32 +0300

    Добавлена форма входа
    Сверстана форма входа и подготовлен метод для отправки на сервер

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
-
+  form = new FormGroup({
+    username: new FormControl(null),
+    password: new FormControl(null),
+  });
+  onSubmit() {
+    console.log(this.form.value)
+  }
 }
