5f2e32853a7d7cc4699fd96e6db3316845cb7ab3
Сергей Захарьев <sergeyz@192.168.0.104>
2025-02-01 22:34:48 +0300

    Update tokens

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,18 +1,61 @@
-import { HttpInterceptorFn} from "@angular/common/http";
+import {HttpHandlerFn, HttpInterceptorFn, HttpRequest} from "@angular/common/http";
 import {inject} from "@angular/core";
 import {AuthService} from "./auth.service";
-import {catchError} from "rxjs";
+import {catchError, switchMap, throwError} from "rxjs";
 
+let isRefreshing = false;
 export const authTokenInterceptor: HttpInterceptorFn = (req, next) =>{
   const accessToken = inject(AuthService).access_token
+  const authService = inject(AuthService)
   if (!accessToken) return next(req);
+  if (isRefreshing) {
+    return refreshAndProceed(authService, req, next);
+  }
   req = req.clone({
     setHeaders: {
       'Authorization': `Bearer ${accessToken}`
     }
   })
-  return next(req).pipe(
+  return next(addToken(req, accessToken)).pipe(
     //dodelat' perehvatchik
-    catchError(err => next(err))
-  )
+    catchError(error => {
+      if (error.status ===403) {
+
+        return refreshAndProceed(authService, req, next);
+
+      }
+      return throwError(error);
+    }
+  ))
+}
+
+const refreshAndProceed = (
+  authService: AuthService,
+  req: HttpRequest<any>,
+  next: HttpHandlerFn) => {
+  if (!isRefreshing) {
+    isRefreshing=true
+    return authService.refreshAuthToken()
+      .pipe(
+        switchMap(res=>{
+          isRefreshing=false;
+          return next(addToken(req, res.access_token))
+        })
+      )
+  }
+  return next(addToken(req, authService.access_token))
+  // return AuthService.refreshAuthToken()
+  //   .pipe(
+  //     switchMap(token => {
+  //       return next(req)
+  //     })
+  //   )
+
+}
+const addToken = (req:HttpRequest<any>, accessToken: string |null ) => {
+  return req.clone({
+    setHeaders: {
+      'Authorization': `Bearer ${accessToken}`
+    }
+})
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,16 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {AuthResponse} from "../data/interfaces/auth.interface";
-import {tap} from "rxjs";
+import {catchError, tap, throwError} from "rxjs";
 import {CookieService} from "ngx-cookie-service";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   http = inject(HttpClient)
+  router = inject(Router)
   baseApiUrl = 'http://127.0.0.1:5001/api/v1/login/auth/';
   cookieService = inject(CookieService)
   access_token: string | null = null;
@@ -18,6 +20,7 @@ export class AuthService {
   get isAuth(){
     if(!this.access_token){
       this.access_token = this.cookieService.get('access_token');
+      this.refresh_token = this.cookieService.get('refresh_token');
     }
     return !!this.access_token;
   }
@@ -37,4 +40,25 @@ export class AuthService {
       })
     )
   }
+  refreshAuthToken(){
+    return this.http.post<AuthResponse>(`${this.baseApiUrl}/refresh_token`, {
+      refresh_token: this.refresh_token
+
+    }).pipe(
+      catchError(err => {
+        this.logout()
+        return throwError(err);
+      })
+    )
+
+  }
+  logout(){
+    this.cookieService.deleteAll()
+    this.access_token = null;
+    this.refresh_token = null;
+    this.user_id = null;
+    this.router.navigate(['/login']);
+  }
+
+
 }
